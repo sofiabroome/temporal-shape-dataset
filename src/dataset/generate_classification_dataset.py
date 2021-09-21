f218c2a18cb44fe584d02b44c834a2acdaa5ab4f
Sofia Broomé <sbroome@kth.se>
2021-09-21 12:04:55 +0200

    refactor to symbol size, adapt dot size

--- src/dataset/generate_classification_dataset.py
+++ src/dataset/generate_classification_dataset.py
@@ -64,7 +64,7 @@ def generate_rectangle(time_steps):
     interval_length = int(time_steps/nb_sides)
     velocities = []
     for i in range(nb_sides):
-        velocities.extend([velocs[i] for j in range(interval_length)])
+        velocities.extend([velocs[i] for _ in range(interval_length)])
     return velocities
 
 
@@ -79,7 +79,7 @@ def get_limits(lims, symbol_width, max_radius, shape=None):
 
 
 def generate_temporal_shape_dataset(training, shape=(64, 64), num_frames=30, num_sequences=2,
-                                    original_size=14, nums_per_image=1,
+                                    symbol_size=14, nums_per_image=1,
                                     max_radius=2, object_mode='dot', save_gifs=True):
     """
     Args:
@@ -87,7 +87,7 @@ def generate_temporal_shape_dataset(training, shape=(64, 64), num_frames=30, num
         shape: Shape we want for our moving images (new_width and new_height)
         num_frames: Number of frames in a particular movement/animation/gif
         num_sequences: Number of movement/animations/gif to generate
-        original_size: Real size of the images (eg: MNIST is 28x28)
+        symbol_size: Real size of the images (eg: MNIST is 28x28)
         nums_per_image: Digits per movement/animation/gif.
     Returns:
         Dataset of np.uint8 type with dimensions
@@ -96,14 +96,13 @@ def generate_temporal_shape_dataset(training, shape=(64, 64), num_frames=30, num
     width, height = shape
 
     labels = []
-    if object_mode == 'dot':
-        original_size = 5
+
     if object_mode == 'mnist':
         mnist = load_mnist(training)
 
     # Get how many pixels can we move around a single image (to fit its width)
-    lims = (x_lim, y_lim) = width - original_size, height - original_size
-    low, high = get_limits(lims, original_size, max_radius)
+    lims = width - symbol_size, height - symbol_size
+    low, high = get_limits(lims, symbol_size, max_radius)
     print(low, high)
     print('lims: ', lims, '\n')
 
@@ -112,12 +111,9 @@ def generate_temporal_shape_dataset(training, shape=(64, 64), num_frames=30, num
     print('Generating sequences...')
     for img_idx in tqdm(range(num_sequences)):
         # Create an array of shape num_sequences x 1 x new_width x new_height
-        # Eg : 3000000 x 1 x 64 x 64
         sequence = np.empty((num_frames, 1, width, height), dtype=np.uint8)
 
-        # label = np.random.randint(len(TemporalShape))
         label = np.random.randint(num_classes)
-        # label = 1
         labels.append(label)
 
         if TemporalShape(label).name == 'CIRCLE':
@@ -128,7 +124,7 @@ def generate_temporal_shape_dataset(training, shape=(64, 64), num_frames=30, num
 
         if TemporalShape(label).name == 'SPIRAL':
             velocities = generate_spiral(time_steps=num_frames, max_radius=10)
-            low, high = get_limits(lims, original_size, max_radius=10, shape='SPIRAL')
+            low, high = get_limits(lims, symbol_size, max_radius=10, shape='SPIRAL')
             # print('adjusted: ')
             # print(low, high, '\n')
 
@@ -141,7 +137,7 @@ def generate_temporal_shape_dataset(training, shape=(64, 64), num_frames=30, num
         if object_mode == 'mnist':
             # Get a list containing three PIL images randomly sampled from the database
             mnist_images = [Image.fromarray(get_image_from_array(mnist, r, mean=0)).resize(
-                (original_size, original_size), Image.ANTIALIAS)
+                (symbol_size, symbol_size), Image.ANTIALIAS)
                 for r in np.random.randint(0, mnist.shape[0], nums_per_image)]
 
         # Generate tuples of (x,y) i.e initial positions for nums_per_image (default : 2)
@@ -164,7 +160,10 @@ def generate_temporal_shape_dataset(training, shape=(64, 64), num_frames=30, num
                 if object_mode == 'dot':
                     x_pos = int(positions[0])
                     y_pos = int(positions[1])
-                    canvas[0, x_pos-2:x_pos+2, y_pos-2:y_pos+2] = 255
+                    dot_radius = int(symbol_size / 2)
+                    canvas[0,
+                           x_pos-dot_radius:x_pos+dot_radius+1,
+                           y_pos-dot_radius:y_pos+dot_radius+1] = 255
 
             # Get the next position by adding velocity
             next_pos = positions + velocities[frame_idx]
@@ -210,24 +209,26 @@ def generate_temporal_shape_dataset(training, shape=(64, 64), num_frames=30, num
 
 
 def main(training, dest, frame_size=64, num_frames=30, num_sequences=2,
-         original_size=14, nums_per_image=1, object_mode='dot', save_gifs=True):
+         symbol_size=14, nums_per_image=1, object_mode='dot', save_gifs=True):
     labels_df = generate_temporal_shape_dataset(
         training, shape=(frame_size, frame_size), num_frames=num_frames,
-        num_sequences=num_sequences, original_size=original_size,
+        num_sequences=num_sequences, symbol_size=symbol_size,
         nums_per_image=nums_per_image, object_mode=object_mode, save_gifs=save_gifs)
     labels_df.to_csv(os.path.join(dest, 'labels.csv'))
 
 
 if __name__ == '__main__':
     num_frames = 20
-    num_sequences = 100000
+    num_sequences = 10
     train_test = 'train'
     object_mode = 'dot'
+    symbol_size = 2
     num_classes = 5
 
     train = True if train_test == 'train' else False
-    dest = f'../../data/classification_{object_mode}_{num_classes}classes_{train_test}_{num_sequences}seqs_{num_frames}_per_seq/'
+    dest = f'../../data/classification_{symbol_size}{object_mode}_{num_classes}classes_{train_test}_{num_sequences}seqs_{num_frames}_per_seq/'
     if not os.path.isdir(dest):
         os.mkdir(dest)
-    main(training=train, dest=dest, num_frames=num_frames, num_sequences=num_sequences, object_mode=object_mode, save_gifs=True)
+    main(training=train, dest=dest, num_frames=num_frames, num_sequences=num_sequences,
+         object_mode=object_mode, symbol_size=symbol_size, save_gifs=True)
 
